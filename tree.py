e4b4c9f12931842548d45cc0db53056a4276a61a
Anthony Gandini <unconfigured@null.spigotmc.org>
2019-09-17 14:27:06 -0400

    update to first commit

--- tree.py
+++ tree.py
@@ -2,7 +2,7 @@
 
 def main():
     x = eval(input("Enter the height you want the tree to be: "))
-    i = int(x / 2)
+    i = int(x)
     count = .5
     whiteSpace = " "
     if(x > 0):
@@ -11,8 +11,8 @@ def main():
             count = count + 1
             i = i - 1
     else:
-        print("\n That number was invalid, please enter a positive integer. \n")
+        print("\nThat number was invalid, please enter a positive integer. \n")
         main()
-    print(int((x / 2)) * whiteSpace , "#")
+    print(int((x)) * whiteSpace , "#")
 
 main()
